c00d589dddd4ea435f97280577342beab58d64c4
John Alade <John.Alade@Interswitchng.com>
2017-03-15 14:29:16 +0100

    Refactorings and Fixings

--- src/js/components/Header.js
+++ src/js/components/Header.js
@@ -3,16 +3,14 @@ import React from "react";
 export default class Header extends React.Component {
 	constructor(){
 		super();
-		this.state = {headerCaption:"Nord Software"};
 	}
 
 	render(){
-		let caption = this.state.headerCaption;
 		return(
 			<div className="header">
 		      <div className="header-left">
 		        <img alt="{caption}" className="header-logo" src="./logo.PNG"/>
-		        <span className="header-text">{caption}</span>
+		        <span className="header-text">Nord Software</span>
 		      </div>
 		      
 		    </div>
